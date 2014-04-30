4b2ebc5152bbcb8e2197690bf0afaef4fef13f89
Lloyd Smith <lloydcjs@gmail.com>
2014-04-30 12:17:42 -0400

    More basics

--- timeseries.py
+++ timeseries.py
@@ -3,6 +3,7 @@ from timeseries_maths import *
 from timeseries_utility import *
 from urllib.request import *
 from datetime import date, datetime, timedelta
+import sys
 
 #Load one year by default
 end_date=date.today()
@@ -28,14 +29,19 @@ def main():
 
 
 def get_ticker(symbol):
+    symbol=symbol.strip().upper()
     for t in tickers:
         if t.symbol==symbol:
             return t
     t = load_ticker(symbol, start_date, end_date)
     tickers.append(t)
+    return t
 
 def do_print(line):
     print(line)
+
+def do_quit(line):
+    sys.exit(0)
 
 def do_default(line):
     print("What!?")
@@ -51,17 +57,18 @@ def do_show(line):
     for t in tickers:
         print(t)
 
-    t = load_ticker("AAPL", start_date, end_date)
+def do_mean(line):
+    symbol, line=next_word(line)
+    t=get_ticker(symbol)
+    print("Mean closing price:", mean(t.close_list()))
 
-    print("Mean closing price: ", mean(t.close_list()))
-    print("Mean volume: ", mean(t.volume_list()))
+def do_lsr(line):
+    symbol, line=next_word(line)
+    t=get_ticker(symbol)
     alpha, beta = linear_regression(t.close_list())
-    print("Alpha: ",alpha," Beta: ",beta)
-    mu, sigma = estimate_mu_sigma(t.close_list(),251)
-    print("mu",mu,"sigma",sigma)
-    bs_out=BS(t.close(-1),290,0,sigma,10)
-    print("BS",bs_out)
-
+    print('Least squares regression', symbol,'closing price')
+    print("Alpha: ",alpha)
+    print("Beta: ",beta)
 
 def do_blah(line):
     print("OK",line)
@@ -73,7 +80,9 @@ commands={
 'print':do_print,
 'load':do_load,
 'show':do_show,
-'blah':do_blah
+'blah':do_blah,
+'quit':do_quit,
+'lsr': do_lsr
 }
 
 if __name__ == '__main__':
